e0798a6cfeece2f1ca3deac4355b01aae95a5286
Ivonei Junior <ivoneijr@gmail.com>
2015-06-10 18:37:58 -0300

    each puts

--- builder.rb
+++ builder.rb
@@ -1,46 +1,26 @@
-require 'json'
-
-file = File.read('produto.json')
-data = JSON.parse(file)
-
-table_name = data['name']
-column = data['columns'].first
-col_name = column['name']
-col_type = column['type']
-col_length = column['length']
-
-col_precision = column['precision']
-col_scale = column['scale']
-
-col_nullable = column['nullable']
+class Builder
+  require 'json'
+  file = File.read('produto.json')
+  data = JSON.parse(file)
 
 
-script = "CREATE TALBE #{table_name} ( #{col_name} #{col_type}(#{col_length}) #{col_nullable})"
+  # attr_accessor :name
+  table_name = data['name']
+  columns = ""
 
- p script
+  data['columns'].each do |column|
 
+    name = column['name']
+    type = column['type']
+    length = column['length']
+    precision = column['precision']
+    scale = column['scale']
+    nullable = column['nullable']
 
+    columns += "#{name} #{type}(#{length}) #{nullable} "
+  end
 
-def create
-  p 'hi'
+  @script = "CREATE TABLE #{table_name} ( #{columns} )"
+  p @script
 end
-# GUIA
-# CREATE TABLE produtos
-# (
-#   codproduto character varying(10) NOT NULL
-# )
-
-
-# data['columns'].each do |column|
-
-#   puts column['name']
-#   puts column['type']
-#   puts column['length']
-#   puts column['precision']
-#   puts column['scale']
-#   puts column['nullable']
-# end
-
-
-
 
