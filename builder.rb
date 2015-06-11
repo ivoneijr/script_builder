587de7b99bad369a6ede7bb19642073cc19cdfd2
Ivonei Junior <ivoneijr@gmail.com>
2015-06-11 13:43:18 -0300

    Building postgres create script from JSON file

--- builder.rb
+++ builder.rb
@@ -1,26 +1,59 @@
-class Builder
-  require 'json'
-  file = File.read('produto.json')
-  data = JSON.parse(file)
+require 'json'
+file = File.read('produto.json')
+data = JSON.parse(file)
 
+@table_name = data['name']
+@columns = ''
+@script
 
-  # attr_accessor :name
-  table_name = data['name']
-  columns = ""
-
-  data['columns'].each do |column|
-
-    name = column['name']
-    type = column['type']
-    length = column['length']
-    precision = column['precision']
-    scale = column['scale']
-    nullable = column['nullable']
-
-    columns += "#{name} #{type}(#{length}) #{nullable} "
+def assert_type(type, length, precision, scale)
+  if type == 'longvarchar'
+    return "character varying(#{length})"
   end
 
-  @script = "CREATE TABLE #{table_name} ( #{columns} )"
-  p @script
+  if type == 'time'
+    return "time with time zone"
+  end
+
+  if type == 'tinyint' || type == 'integer'
+    return "integer"
+  end
+
+  if type == 'char'
+    return "\"char\""
+  end
+
+  if type == 'decimal'
+    return "numeric(#{precision},#{scale})"
+  end
+
+  if type == 'date'
+    return "date"
+  end
+
 end
 
+data['columns'].each_with_index do |column, index|
+
+  name      = column['name']
+  type      = column['type']
+  length    = column['length']
+  precision = column['precision']
+  scale     = column['scale']
+  nullable  = column['nullable']
+
+  formated_type = assert_type(type,length, precision, scale)
+  nullable ? nullable = 'NOT NULL' : nullable = ''
+
+  unless (index == data['columns'].size-1)
+    @columns << "#{name} #{formated_type} #{nullable},"
+  else
+    @columns << "#{name} #{formated_type} #{nullable}"
+  end
+
+end
+
+
+
+@script = "CREATE TABLE #{@table_name} ( #{@columns} )"
+p @script
---
+++ test.rb
@@ -0,0 +1,8 @@
+def assert_type
+  return "banana"
+end
+
+
+variavel = assert_type
+
+p variavel
